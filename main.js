19195ec29048d5025797ec3ca80cc893c0700aa3
saidmuhamedovsaidakbar27@gmail.com <Brat711>
2024-11-06 11:08:42 +0500

    go

---
+++ main.js
@@ -0,0 +1,9 @@
+const userName = prompt("Ismingizni kriting")
+const surName = prompt("Spacer idingizni kritin")
+const idName = prompt("Space passwordingizni kriting")
+const infol = {
+    ism:userName,
+    idi:surName,
+    password:idName,
+}
+console.log(infol);
